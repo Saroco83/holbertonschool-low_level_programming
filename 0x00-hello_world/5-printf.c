099c41a2ce25e778cf449314e657fef97a09b917
Saroco83 <estudialondres@gmail.com>
2020-02-05 21:15:37 +0000

    printf plus betty plus Wall

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function ppal
+ *
+ * Return: Value of succes
+ *
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
