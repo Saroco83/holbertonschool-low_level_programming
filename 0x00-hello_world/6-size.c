4f5565bd4a8e45f4e0db437541076d0dfda05806
Saroco83 <estudialondres@gmail.com>
2020-02-05 23:00:27 +0000

    sizes in 32 and 64 bits

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+/**
+ * manin - ppal function
+ *
+ * return: 0 on succes
+ *
+ */
+
+printf("Size of char: %i byte(s)\n", sizeof(char));
+printf("Size of int: %d byte(s)\n", sizeof(int));
+printf("Size of long int: %d byte(s)\n", sizeof(long int));
+printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of float: %i byte(s)\n", sizeof(float));
+return (0);
+}
