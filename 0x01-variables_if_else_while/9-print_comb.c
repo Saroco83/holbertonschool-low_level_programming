cf72a74e6d86f2ccc2cc679736dc5ddd6a5a9c07
Saroco83 <estudialondres@gmail.com>
2020-02-06 23:32:46 +0000

    numbers plus comma and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - function ppal.
+ * Return: 0 on succes.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
