71e2c48d58e51d13dd8bc5c732f7fca8c06c608d
Saroco83 <estudialondres@gmail.com>
2020-02-06 22:43:15 +0000

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - function ppal.
+ * Return: 0 on succes.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
