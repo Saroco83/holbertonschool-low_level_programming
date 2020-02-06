669e3cfd19671f5eeee971bdb1d2258db951dce6
Saroco83 <estudialondres@gmail.com>
2020-02-06 20:01:02 +0000

    alphabet with char variable

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - functim ppal.
+ *
+ * Return: succes 0.
+ *
+ */
+int main(void)
+{
+	char a = 97;
+
+	for (; a < 122; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
