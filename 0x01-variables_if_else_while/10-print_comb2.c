0c1393c989888826cbc3f2fc5a8fce38a24b4f2b
Saroco83 <estudialondres@gmail.com>
2020-02-07 21:37:23 +0000

    00 to 99 with and no at the end

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -8,13 +8,13 @@ int main(void)
 	int i, j;
 
 	for (i = 48; i <= 57; i++)
-	{		
+	{
 		for (j = 48; j <= 57; j++)
 		{
 		putchar(i);
 		putchar(j);
 
-			if 
+			if
 			(i != 57 || j != 57)
 			{
 				putchar(44);
