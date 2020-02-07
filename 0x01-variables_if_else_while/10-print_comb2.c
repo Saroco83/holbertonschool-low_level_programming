92a6626978c1dbf6416ca502c8cf6d6ebec14b35
Saroco83 <estudialondres@gmail.com>
2020-02-07 01:22:10 +0000

    draft

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - function ppal.
+ * Return: 0 on succes.
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{	putchar(i);
+	putchar(j);
+		for (j = '0'; j <= '9'; i++)
+*		{
+*		*		/**	if 
+*		*	(i != '9' && j != '9')
+*		*	{
+*		*		putchar(44);
+*		*		putchar(32);
+*		*	
+*		}
+*/	}
+	putchar('\n');
+	return (0);
+}
