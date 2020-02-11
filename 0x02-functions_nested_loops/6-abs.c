36b8da2af95f7c24dbbb3d60c80008b2014726fa
Saroco83 <estudialondres@gmail.com>
2020-02-11 19:57:42 +0000

    value absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _abs - absolute value fuction.
+ * Return: integer 0 on succes.
+ * @a: parameter integer.
+ */
+
+int _abs(int a)
+{
+
+	int b;
+
+	if (a < 0)
+	{
+		b = -1 * a;
+		return(b);
+	}
+	return(a);
+}
