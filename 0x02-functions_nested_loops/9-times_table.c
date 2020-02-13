7cc21d1811b5d3d3b9b0962eb7e8e4903153f665
Saroco83 <estudialondres@gmail.com>
2020-02-13 05:37:55 +0000

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,13 +6,30 @@
  */
 void times_table(void)
 {
-	int x = 0, y = 0, z= 0;
+	int z = 0, x = 0, y = 0;
 	
 	for (y = 0; y <= 9; y++)
 	{
 		for (x = 0; x <= 9; x++)
 		{
 			z = y * x;
+			if(z >= 10 )
+			{
+				_putchar((z/10) + 48);
+				_putchar((z%10) + 48);
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(z + 48);
+			}
+			if(x != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			
 		}
+		_putchar('\n');
 	}
 }
