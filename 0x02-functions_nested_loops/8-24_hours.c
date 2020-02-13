9ddee0048831f2d1e2bed9486bdfe198d2fc5671
Saroco83 <estudialondres@gmail.com>
2020-02-13 00:29:54 +0000

    24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,8 +2,6 @@
 /**
  * jack_bauer - print the 24 hours
  *
- *
- *
  */
 void jack_bauer(void)
 {
@@ -11,19 +9,21 @@ void jack_bauer(void)
 
 	for (a = 0; a <= 2; a++)
 	{
-		for(b = 0; b <= 9; b++)
+		for (b = 0; b <= 9; b++)
 		{
-			for(c = 0; c <= 5; c++)
+			for (c = 0; c <= 5; c++)
 			{
-				for(d = 0; d <= 9; d++)
+				for (d = 0; d <= 9; d++)
 				{
-			/**		if( a != 2 && b != 3 && c > 6 && d != 9 )
-			*/		_putchar(a + 48 );
-					_putchar(b + 48);
-					_putchar(':');
-					_putchar(c + 48);
-					_putchar(d + 48);
-					_putchar('\n');
+					if (!(a == 2 && b > 3))
+					{
+						_putchar(a + 48);
+						_putchar(b + 48);
+						_putchar(':');
+						_putchar(c + 48);
+						_putchar(d + 48);
+						_putchar('\n');
+					}
 				}
 			}
 		}
