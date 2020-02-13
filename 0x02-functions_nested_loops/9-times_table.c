5970176ae2eaa1bc1cb4bf421d81f414dcf90a2d
Saroco83 <estudialondres@gmail.com>
2020-02-13 01:05:42 +0000

    advance

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ */
+void times_table(void)
+{
+	int x = 0, y = 0, z= 0;
+	
+	for (y = 0; y <= 9; y++)
+	{
+		for (x = 0; x <= 9; x++)
+		{
+			z = y * x;
+		}
+	}
+}
