b5ab9d6d8ff95ca8c44dc7fbd3e392e7475ff995
Saroco83 <estudialondres@gmail.com>
2020-02-21 13:32:41 +0000

    string copy

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _strcmp - function to compare
+ * @s1: string 1 to compare
+ * @s2: string 2 to compare
+ * Return: integer. 0 on succes != of 0 if diferent
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, j, z;
+	int *H2 = null;
+	int *H1 = null;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		*H1 += s1[i];
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		*H2 += s2[j];
+	}
+	z = *H1 + *H2;
+	return(z);
+}
