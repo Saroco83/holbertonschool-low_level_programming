c420b9c868bc8a1d1d2cedd124b89982da9e8976
Saroco83 <estudialondres@gmail.com>
2020-02-24 05:37:21 +0000

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,25 +1,25 @@
 #include "holberton.h"
-/*
- * cap_string - Change value before special key to upper
+/**
+ * cap_string - capitalizes all words of a string.
  * @s: pointer to string parameter
  * Return: s pointer
  */
 char *cap_string(char *s)
 {
-	char kys[14] = {',', ';', '.', '!', '?', '"','(', ')', '{', '}', '\n', '\t', ' '};
+	char k[14] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n', '\t', ' '};
 	int  a = 0, b = 0;
 
 	while (s[a] != '\0')
 	{
 		if (s[0] >= 'a' && s[0] <= 'z')
 			s[0] -= 32;
-		while (kys[b] != '\0')
+		while (k[b] != '\0')
 		{
-			if(s[a] == kys[b])
+			if (s[a] == k[b])
 			{
 				if ((s[a + 1] >= 'a') && (s[a + 1] <= 'z'))
 				{
-					s[a + 1] = s[a + 1]-32;
+					s[a + 1] = s[a + 1] - 32;
 				}
 			}
 			b++;
