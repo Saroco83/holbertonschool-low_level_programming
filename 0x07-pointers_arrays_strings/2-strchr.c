8160923265a4a5272f9647be805e2cf3fb1147b9
Saroco83 <estudialondres@gmail.com>
2020-02-24 21:58:08 +0000

    pointer to char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _strchr - function to return pointer to char
+ * @s: pointer parameter to string
+ * @c: value of char
+ * Return: pointer s
+ */
+char *_strchr(char *s, char c)
+{
+	int count = 0;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		if (s[count] == c)
+		{
+			s = &s[count];
+			return (s);
+		}
+	}
+	return ('\0');
+}
