73e3fa1b55d5c78f0bf1b99e5d77f189cfd13b08
Saroco83 <estudialondres@gmail.com>
2020-02-26 23:59:31 +0000

    natural root square

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,31 @@
 #include "holberton.h"
 #include <stdio.h>
-/**
- *
- *
- *
- */
 
+int sqrt_parmtr(int n, int parmtr);
+/**
+ * sqrt_parmtr - recursive function
+ * @n: parameter 1
+ * @parmtr: parameter to str the iteration
+ * Return: value of square
+ */
+int _sqrt_recursion(int n)
+{
+	int parmtr = 1;
+	int m;
+
+	m = sqrt_parmtr(n, parmtr);
+	return (m);
+}
+int sqrt_parmtr(int n, int parmtr)
+{
+	if (parmtr > n)
+	{
+		return (-1);
+	}
+	if (parmtr * parmtr == n)
+	{
+		return (parmtr);
+	}
+	else
+	return (sqrt_parmtr(n, parmtr  + 1));
+}
