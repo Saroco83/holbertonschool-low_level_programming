d1ff9c0262506d8fb8249b737f341efabfc910b5
Saroco83 <estudialondres@gmail.com>
2020-02-26 21:36:48 +0000

    recursion factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,8 +1,23 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *
- *
- *
+ * factorial - function to recursion
+ * @n: parameter of integer to calculate factorial
+ * Return: integer
  */
-
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n > 0)
+	{
+	return (factorial(n - 1) * n);
+	}
+	return (0);
+}
