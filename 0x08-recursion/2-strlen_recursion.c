8e9ce189f811ab1be8b3caa1bc9524f4d70eae3f
Saroco83 <estudialondres@gmail.com>
2020-02-26 21:03:36 +0000

    Lengh of strin with recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,8 +1,17 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *
- *
- *
+ * _strlen_recursion - fuction with recursion to count the lengh
+ * @s: pointer parameter to string
+ * Return: len inter of lengh string
  */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
 
+	if (*s != '\0')
+	{
+	return (_strlen_recursion(s + 1) + 1);
+	}
+	return (len);
+}
