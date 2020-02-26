ab453359ceafae71aec1dc7f8e83ceb4731c703c
Saroco83 <estudialondres@gmail.com>
2020-02-26 18:43:07 +0000

    Reverse recursive printing

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,9 +1,14 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *
- *
- *
+ * _print_rev_recursion - reverse printning recursive
+ * @s: pointer parameter to string
  */
 void _print_rev_recursion(char *s)
-ll
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
