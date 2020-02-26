e5ce99b169df2dbdcbfe14256bed9942701d9bf7
Saroco83 <estudialondres@gmail.com>
2020-02-26 22:15:53 +0000

    power recursive

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,8 +1,24 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *
- *
- *
+ * _pow_recursion - recursive function
+ * @x: base to be raised
+ * @y: number to be raised
+ * Return: integer
  */
-
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y > 0)
+	{
+		return (_pow_recursion(x, y - 1) * x);
+	}
+	return (0);
+}
