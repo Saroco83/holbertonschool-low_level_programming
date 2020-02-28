d32f047135ec167f83e37d172eba75f52ca514ee
Saroco83 <estudialondres@gmail.com>
2020-02-28 01:03:20 +0000

    notes for 7th task palindrome

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,8 +1,35 @@
 #include "holberton.h"
 #include <stdio.h>
+int _strlen_recursion(char *s);
 /**
  *
  *
  *
  */
+int is_palindrome(char *s)
+{
+	
+	int len = 0;
 
+	len = _strlen_recursion(s);
+	if (len = 0)
+	{
+		return (1);
+	}
+	pali_rec_parmtr(s + 1, len);
+}
+
+
+
+
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+        if (*s != '\0')
+        {
+        return (_strlen_recursion(s + 1) + 1);
+        }
+        return (len);
+}
