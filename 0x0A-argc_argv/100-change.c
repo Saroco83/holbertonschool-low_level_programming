6f073bb8f22afd16a765ef65cc923908879945b4
Saroco83 <estudialondres@gmail.com>
2020-03-01 20:44:07 +0000

    coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,12 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+int funaux(int i);
 /**
- * main - function ppal.
- * @argc: counter.
- * @argv: values, strings.
- * Return: integer value.
+ *funaux - coins counter.
+ *@i: money.
+ *Return: coin number.
+ */
+int funaux(int i)
+{
+	int c, mc = 0;
+	int monedas[] = {25, 10, 5, 2, 1};
+	
+	for (c = 0; i != 0;)
+	{
+		fun:;
+		if (i - monedas[c] >= 0)
+		{
+			i = i - monedas[c];
+			mc++;
+		}
+		else
+		{
+			if (i > 0)
+			{
+				c++;
+				goto fun;
+			}
+		return (mc);
+		}
+	}
+	return (0);
+}
+
+/**
+ *main -  prints the minimum number of coins to make change.
+ *@argc: number of arguments passed.
+ *@argv: pointer array which points to each argument passed.
+ *Return: return 0 if all its good and 1 if not receive two arguments.
  */
 int main(int argc, char *argv[])
 {
+	int i;
 
-
+	if (argc == 2)
+	{
+		i = atoi(argv[1]);
+		if (i < 0)
+		{
+			printf("0\n");
+		}
+		else
+		{
+			printf("%d\n", funaux(i));
+		}
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
