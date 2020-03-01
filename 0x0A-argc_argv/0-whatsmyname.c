698d58c645f69b9640715585534d42e08a155d34
Saroco83 <estudialondres@gmail.com>
2020-03-01 18:08:15 +0000

    changes to indentation and spaces

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - function ppal.
- * @argc: counter.
- * @argv: values, strings.
- * Return: integer value.
+ * main - function ppal
+ * @argc: counter
+ * @argv: values, strings
+ * Return: integer value
  */
 int main(int argc, char *argv[])
 {
 	if (argc >= 0)
 		printf("%s\n", argv[0]);
-	
+
 	return (0);
 
 }
