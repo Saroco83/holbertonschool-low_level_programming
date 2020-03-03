f4003ff6a1ed144fef0f2010c045868c7cbc3842
Saroco83 <estudialondres@gmail.com>
2020-03-03 21:45:14 +0000

    copy array to new one allocating memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
- *
- *
+ * _strdup - function to creates and fill array
+ * @str: pointer to array source
+ * Return: pointer to new array filled
  */
+char *_strdup(char *str)
+{
+	int fill, len = 0;
+	char *newstr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for ( ; str[len] != '\0'; len++)
+	{}
+	newstr = malloc(len * sizeof(char));
+	if (newstr == NULL)
+	{
+		return (NULL);
+	}
+	for (fill = 0; fill < len; fill++)
+	{
+		newstr[fill] = str[fill];
+	}
+	return (newstr);
+}
