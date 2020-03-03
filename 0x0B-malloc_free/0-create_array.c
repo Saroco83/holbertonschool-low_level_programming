3d02a476a55cc717e3b3aacb2f6dcdd5189b4aaa
Saroco83 <estudialondres@gmail.com>
2020-03-03 18:56:25 +0000

    allocate memory for array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ *
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size == NULL)
+	{
+		return(NULL);
+	}
+	return(size* (malloc(unsigned int)));
+}
