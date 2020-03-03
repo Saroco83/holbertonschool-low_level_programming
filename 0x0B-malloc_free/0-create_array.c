8739e29507ec87986c5616f0dbd4b7cc98928721
Saroco83 <estudialondres@gmail.com>
2020-03-03 20:08:24 +0000

    allocate memory for array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
- *
- *
+ * create_array - allocation function
+ * @size: array size
+ * @c: char to initialize
+ * Return: pointer to memory allocated
  */
 char *create_array(unsigned int size, char c)
 {
-	if (size == NULL)
+	char *array;
+	unsigned int i;
+
+	array = malloc(size * sizeof(char));
+	if (size == 0)
 	{
-		return(NULL);
+		return (NULL);
 	}
-	return(size* (malloc(unsigned int)));
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
 }
