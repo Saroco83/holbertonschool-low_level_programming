61bd86b543837f5cdac5a238071816e60bc7bf52
Saroco83 <estudialondres@gmail.com>
2020-03-04 22:33:06 +0000

    retur 2 strings in one pointer

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,37 +8,36 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-        char *concat = NULL;
-        int fill2 = 0, len2 = 0, len1 = 0, fill, lenAdded = 0;
+	char *concat = NULL;
+	int fill2 = 0, len2 = 0, len1 = 0, fill, lenAdded = 0;
 
-        if (s1 == NULL) 
-        {
-                s1 = "";
-        }
-        
-        if (s2 == NULL)
-        {
-                s2 = "";
-        }
-        for (len1 = 0; s1[len1] != '\0'; len1++)
-        {}
-        for (len2 = 0; s2[len2] != '\0'; len2++)
-        {}
-        lenAdded = len1 + len2; 
-        concat = malloc((lenAdded + 1) * sizeof(char));
-        if (concat == NULL)
-        {
-                return (NULL);
-        }
-        for (fill = 0; fill < len1; fill++)
-        {
-                concat[fill] = s1[fill];
-        }
-        for (fill2 = 0; s2[fill2] != '\0'; fill2++)
-        {
-                concat[fill + fill2] = s2[fill2];
-        }
-        concat[fill + fill2] = '\0';
-        return (concat);
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+	{}
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+	{}
+	lenAdded = len1 + len2;
+	concat = malloc((lenAdded + 1) * sizeof(char));
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+	for (fill = 0; fill < len1; fill++)
+	{
+		concat[fill] = s1[fill];
+	}
+	for (fill2 = 0; s2[fill2] != '\0'; fill2++)
+	{
+		concat[fill + fill2] = s2[fill2];
+	}
+	concat[fill + fill2] = '\0';
+	return (concat);
 }
-
