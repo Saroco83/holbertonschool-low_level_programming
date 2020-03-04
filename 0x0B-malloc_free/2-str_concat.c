f1629d7035fbcb9aedad0b4a0d8db6b19d3cd5cd
Saroco83 <estudialondres@gmail.com>
2020-03-04 01:06:00 +0000

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - function to creates and fill array
+ * @s1: pointer to array source
+ * @s2: pointer to array
+ * Return: pointer to new array filled
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *concat = NULL;
+	int fill2 = 0, len2 = 0, len1 = 0, fill;
 
+	if ((s1 == NULL) && (s2 == NULL))
+	{
+		return (NULL);
+	}
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+	{}
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+	{}
+	concat = malloc((len1 + len2 + 1) * sizeof(char));
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+	for (fill = 0; fill < len1; fill++)
+	{
+		concat[fill] = s1[fill];
+	}
+	for (fill = len1; fill <= (len1 + len2); fill++)
+	{
+		concat[fill] = s2[0 + fill2];
+	fill2++;
+	}
+
+	return (concat);
+}
