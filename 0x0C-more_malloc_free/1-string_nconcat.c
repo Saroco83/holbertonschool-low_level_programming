8f2cc537bd52b8346180c3aa01c661fdd273ef1a
Saroco83 <estudialondres@gmail.com>
2020-03-05 23:29:06 +0000

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,38 +9,34 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int fill1, fill2, size2ndstr;
-	unsigned int countS1, countS2;
+	unsigned int fill1, countS1, countS2;
 	char *secondstr;
 
-	if (s1 == NULL)
+	if (s1 == '\0')
 	{
 		s1 = "";
 	}
-	if (s2 == NULL)
+	if (s2 == '\0')
 	{
 		s2 = "";
 	}
 	for (countS1 = 0; s1[countS1] != '\0'; countS1++)
+	{}
 	for (countS2 = 0; s2[countS2] != '\0'; countS2++)
-	if (n <= (countS2))
+	{}
+	if (n >= countS2)
 	{
-		secondstr = malloc(sizeof(char) * (countS1 + n + 1));
-		if (secondstr == NULL)
-			return ("");
-		size2ndstr = countS1 + n;
+		n = countS2;
 	}
-	else
-	{
-		secondstr = malloc(sizeof(char) * (countS1 + countS2 + 1));
-		if (secondstr == NULL)
-			return (NULL);
-		size2ndstr = countS1 + countS2;
+	secondstr = malloc(sizeof(char) * (countS1 + n + 1));
+	if (secondstr == NULL)
+	{	
+		return (NULL);
 	}
 	for (fill1 = 0; s1[fill1] != '\0'; fill1++)
 		secondstr[fill1] = s1[fill1];
-	for (fill2 = 0; (fill1 + fill2) <= size2ndstr; fill2++)
-		secondstr[fill1 + fill2] = s2[fill2];
-	secondstr[fill1 + fill2] = '\0';
+	for (fill1 = 0; fill1 < n; fill1++)
+		secondstr[fill1 + countS1] = s2[fill1];
+	secondstr[countS1 + fill1] = '\0';
 	return (secondstr);
 }
