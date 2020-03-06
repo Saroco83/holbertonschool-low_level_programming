a78be72fb889daa333b8d27f02f3583cd95b7882
Saroco83 <estudialondres@gmail.com>
2020-03-06 01:06:32 +0000

    array filled

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
- *
+ * array - function to create array
+ * @min: fewer number
+ * @max: higher number
+ * Return: pointer to integer
  */
+int *array_range(int min, int max)
+{
+	int *leo = NULL;
+	int i, size;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	size = max - min + 1;
+	leo = malloc(size * 4);
+	if (leo == NULL)
+	{
+		return (NULL);
+	}
+	for (i = min; i <= max; i++)
+	{
+		leo[i] = i;
+	}
+	return (leo);
+}
