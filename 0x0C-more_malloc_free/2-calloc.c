9855e4e5fe4ef16765a07e9b8b3741690b28fca6
Saroco83 <estudialondres@gmail.com>
2020-03-06 00:23:30 +0000

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,7 +1,33 @@
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
- *
+ * _calloc - function emulated
+ * @nmemb: amounth of bytes
+ * @size: bytes of variable type
+ * Return: pointer void type
  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *array = NULL;
+	unsigned int i = 0;
+
+	if (nmemb == 0)
+	{
+		return (0);
+	}
+	if (size == 0)
+	{
+		return (0);
+	}
+	array = malloc(nmemb * size);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= nmemb; i++)
+	{
+		array[i] = 0;
+	}
+	return (array);
+}
