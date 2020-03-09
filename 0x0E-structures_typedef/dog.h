7b1d4cc0d9e15da85ce73d5fbb0f29b96c9acd9e
Saroco83 <estudialondres@gmail.com>
2020-03-09 21:57:31 +0000

    type def

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOGGY
+#define DOGGY
+/**
+ * struct dog - Structure for the doggy
+ * @name: name of dog
+ * @age: doggy old
+ * @owner: name of human
+ *
+ * Description: This type definition is to use in the structs
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
