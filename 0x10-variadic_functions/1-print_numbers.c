055a1538dcd4a553105b2928ff6a200019b2784f
Saroco83 <estudialondres@gmail.com>
2020-03-13 23:58:13 +0000

    Va-list integer number

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,7 +1,28 @@
 #include "variadic_functions.h"
-
+#include <stdio.h>
+#include <stdarg.h>
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_numbers - function to print n integersas parameters.
+ * @separator: pointer to string.
+ * @n: number of parameters.
  */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list values;
+	unsigned int i = 0;
+
+	va_start(values, n);
+	if (separator)
+	{
+		while (i < n)
+		{
+			printf("%d", (va_arg(values, unsigned int)));
+			if (i < (n - 1))
+			{
+				printf("%s", separator);
+			}
+			i++;
+		}
+	}
+	printf("\n");
+}
