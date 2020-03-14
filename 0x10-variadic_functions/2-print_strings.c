297c66b7daf20b050dac9235497187d19f6f00ff
Saroco83 <estudialondres@gmail.com>
2020-03-14 21:03:34 +0000

    print string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,7 +1,42 @@
 #include "variadic_functions.h"
-
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_strings - function to print string.
+ * @separator: pointer to string
+ * @n: number of parameters
  */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *c = NULL;
+	va_list string;
+
+	va_start(string, n);
+	if (separator)
+	{
+		for (i = 0; i < n; i++)
+		{
+			c = va_arg(string, char*);
+			if (c)
+			{
+				printf("%s",c);
+				if (i < (n - 1))
+				{
+					printf("%s", separator);
+				}
+			}
+			else
+			{
+				printf("(nil)");
+				if (i < (n - 1))
+				{
+					printf("%s", separator);
+				}
+			}
+		}
+	}
+	va_end(string);
+	printf("\n");
+}
