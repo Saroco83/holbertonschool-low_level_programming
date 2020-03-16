9dc1ffc18491e9c7346812fb15dddc633251e028
Saroco83 <estudialondres@gmail.com>
2020-03-16 08:23:51 +0000

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,34 +1,37 @@
-#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_all - functio to execute
+ * @format: list
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0;
+	int i = 0, j = 0;
 	va_list arguments;
 	char *str = NULL;
-	
+
 	va_start(arguments, format);
+	while (format[j] != '\0')
+	{
+		j++;
+	}
 	while (format[i])
 	{
-		switch(format[i])
+		switch (format[i])
 		{
 			case 'c':
-			printf("%c", (char)va_arg(va_list, int));
+			printf("%c", (char)va_arg(arguments, int));
 			break;
 			case 'i':
-			printf("%i", (va_arg(va_list, int)));
+			printf("%i", va_arg(arguments, int));
 			break;
 			case 'f':
-			printf("%f", (float)va_arg(va_list, int));
+			printf("%f", (float)va_arg(arguments, int));
 			break;
 			case 's':
-			if(str == NULL)
+			str = va_arg(arguments, char *);
+			if (str == NULL)
 			{
 				printf("(nil)");
 				break;
@@ -36,6 +39,13 @@ void print_all(const char * const format, ...)
 			printf("%s", str);
 			break;
 		}
-		
+		if ((format[i] == 'c' || format[i] == 'i' ||
+			format[i] == 'f' || format[i] == 's') && (i < (j - 1)))
+		{
+			printf(", ");
+		}
+		i++;
 	}
+	va_end(arguments);
+	printf("\n");
 }
