8800ffe05b5498e7508f1a8462ace166962e6b53
Saroco83 <estudialondres@gmail.com>
2020-03-27 04:09:56 +0000

    setting

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,7 +2,20 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * set_bit - setsthe value of a bit to 1.
+ * @n: number to evaluate
+ * @index: position returned
+ * Return: 1 on succes.
  */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > (sizeof(unsigned long int) * sizeof(unsigned int)))
+	{
+		return (-1);
+	}
+
+	if (!((*n << index) & 1))
+	*n ^= (1 << index);
+
+	return (1);
+}
