25055850bfd58676876ca52fe4de66a53df77c11
Saroco83 <estudialondres@gmail.com>
2020-03-27 00:41:20 +0000

    pow

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,7 +2,21 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_binary - function to print.
+ * @n: number to print.
  */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	if (n & 1)
+	{
+		_putchar(1 + 48);
+	}
+	else
+	{
+		_putchar(0 + 48);
+	}
+}
