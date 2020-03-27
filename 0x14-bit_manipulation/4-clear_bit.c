a23fee7f2f65ce4f022fe9442bf5268a66c4b2e7
Saroco83 <estudialondres@gmail.com>
2020-03-27 04:26:44 +0000

    update

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,7 +2,21 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * clear_bit - sets the value.
+ * @n: number to evaluate
+ * @index: value tu retur
+ * Return: 1 on succes
  */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > (sizeof(unsigned long int) * sizeof(unsigned int)))
+	{
+		return (-1);
+	}
+	if (!((*n << index) & 0))
+	{
+		*n = *n & ~(11 << index);
+	}
+
+	return (1);
+}
