2782103d679c3a23aba387d59e7a3f114ac715fb
Saroco83 <estudialondres@gmail.com>
2020-03-27 03:49:03 +0000

    update

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,7 +2,23 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * get_bit - function to excute.
+ * @n: firsth parameter
+ * @index: value to evaluate
+ * Return: integer
  */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > (sizeof(unsigned long int) * sizeof(unsigned int)))
+	{
+		return (-1);
+	}
+	if ((n >> index) & 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
