36671136182321c1ef5f009fdc7533b362a64ef4
Saroco83 <estudialondres@gmail.com>
2020-04-24 06:58:00 +0000

    palindrome

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,35 +1,50 @@
 #include "holberton.h"
 #include <stdio.h>
-int _strlen_recursion(char *s);
+int end(char *t);
+int combine(char *s, int n);
 /**
- *
- *
- *
- */
+* is_palindrome - Palin
+* @s: Parameter string
+*
+* Return: int
+*
+*/
 int is_palindrome(char *s)
 {
-	
-	int len = 0;
-
-	len = _strlen_recursion(s);
-	if (len = 0)
-	{
-		return (1);
-	}
-	pali_rec_parmtr(s + 1, len);
+int n = end(s);
+if (n <= 1)
+return (1);
+return (combine(s, n));
 }
 
-
-
-
-
-int _strlen_recursion(char *s)
+/**
+* end - Set pinter in end string
+* @t: Pointer parameter
+* Return: int
+*/
+int end(char *t)
 {
-	int len = 0;
+if (*t == '\0')
+{
+return (0);
+}
+t++;
+return (1 + end(t));
+}
 
-        if (*s != '\0')
-        {
-        return (_strlen_recursion(s + 1) + 1);
-        }
-        return (len);
+/**
+* combine - Combine
+* @s: String
+* @n: Size of string
+* Return: :smile:
+*/
+int combine(char *s, int n)
+{
+if (n <= 1)
+return (1);
+else if (*s == *(s + n - 1))
+{
+return (combine(s + 1, n - 2));
+}
+return (0);
 }
