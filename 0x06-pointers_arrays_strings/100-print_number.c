a7aa6fe8261c0780a049419c65cf0de60ce976d6
Saroco83 <estudialondres@gmail.com>
2020-04-24 05:16:28 +0000

    update

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * print_number - description
+ * @n: descript
+ * Return: -
+ */
+void print_number(int n)
+{
+	int a = n;
+	char b = 48;
+	
+	if (a / 10 == 0)
+	{
+		if (a < 0)
+		_putchar(45);
+		_putchar(abs(a) + b);
+		return;
+	}
+	print_number(a / 10);
+	b = b + (abs(a % 10));
+	_putchar(b);
+}
