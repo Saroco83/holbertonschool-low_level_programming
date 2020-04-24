dc1eecae3fe2be1b55f00ffddff6bcf4ca2077ff
Saroco83 <estudialondres@gmail.com>
2020-04-24 08:40:48 +0000

    fix

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,32 +1,68 @@
+#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "calc.h"
 
-op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
 /**
+ * op_add - entry point
+ * @a: int variable
+ * @b: int variable
  *
- *
- *
- *
- */
-int (*get_op_func(char *s))(int, int)
+ * Return: 0
+*/
+
+int op_add(int a, int b)
 {
-	int i;
-	
-	while ( i < 5)
-	{
-		if (ops[i].ops[0] == *s)
-		{
-			return(ops[s].ops[1])
-		}
+	return (a + b);
+}
 
+/**
+ * op_sub - entry point
+ * @a: int variable
+ * @b: int variable
+ *
+ * Return: 0
+*/
 
-	}
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - entry point
+ * @a: int variable
+ * @b: int variable
+ *
+ * Return: 0
+*/
+
+int op_mul(int a, int b)
+{
+	return ((a) * (b));
+}
+
+/**
+ * op_div - entry point
+ * @a: int variable
+ * @b: int variable
+ *
+ * Return: 0
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - entry point
+ * @a: int variable
+ * @b: int variable
+ *
+ * Return: 0
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
 }
