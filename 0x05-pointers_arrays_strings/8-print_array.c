72d166469bae801947097f022aad89c7327968a2
Saroco83 <estudialondres@gmail.com>
2020-04-24 04:51:29 +0000

    array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,27 +1,25 @@
 #include "holberton.h"
+#include <stdio.h>
+
 /**
- * print_number - writes the character c to stdout
- * @n: The character to print
- *
- * On error, -1 is returned, and errno is set appropriately.
- */
-void print_number(int n)
+* print_array - Entry point
+* @a: int variable
+* @n: int variable
+*
+* Return: Always 0 (Success)
+*/
+
+void print_array(int *a, int n)
 {
-	if (n > -10 && n < 10)
+	int i;
+
+	for (i = 0; i < n; i++)
 	{
-		if (n < 0)
-		{
-		_putchar('-');
-		n = n * -1;
-		}
-		_putchar(n + '0');
+		if ((i + 1) == n)
+			printf("%d", a[i]);
+		else
+			printf("%d, ", a[i]);
 	}
-	else
-	{
-		print_number(n / 10);
-		n = n % 10;
-		if (n < 0)
-			n = n * -1;
-		_putchar(n + '0');
-	}
+
+	printf("\n");
 }
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+void print_array(int *a, int n);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
