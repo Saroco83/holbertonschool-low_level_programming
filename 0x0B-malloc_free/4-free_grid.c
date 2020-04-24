01dd475fbfe607ae046191176fce29ebc001284e
Saroco83 <estudialondres@gmail.com>
2020-04-24 07:30:48 +0000

    fix

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,8 +1,16 @@
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
- *
- *
- */
+ * free_grid - funcion de concatenates
+ * @grid: intero pointer de  ponter
+ * @height: tama;o
+*/
+void free_grid(int **grid, int height)
+{
+	int a;
+
+	for (a = 0; a < height; a++)
+		free(grid[a]);
+	free(grid);
+}
