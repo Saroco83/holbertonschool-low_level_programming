13f41342e5b3abf9e8fa75c24d73f50116335594
Saroco83 <estudialondres@gmail.com>
2020-04-24 01:13:26 +0000

    till 98 witout comma

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+/*
+ * print_to_98 - function
+ * @n: parameter of value
+ * Return: 1 on succes or -1 if fails
+ *
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d\n", n);
+		}
+	}
+	else
+	{
+		for ( ; n >= 98; n--)
+		{
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d\n", n);
+		}
+	}
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+void print_to_98(int n);
 void print_alphabet(void);
 int _putchar(char c);
 void print_alphabet_x10(void);
