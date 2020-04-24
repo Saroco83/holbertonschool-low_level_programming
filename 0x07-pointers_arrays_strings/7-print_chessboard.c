eb128117d84d08f0530d34a5fb726ca2b44a7e0a
Saroco83 <estudialondres@gmail.com>
2020-04-24 06:16:32 +0000

    fix

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,18 +1,21 @@
 #include "holberton.h"
+
 /**
- * print_chessboard - writes the character c to stdout
- * @a: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+* print_chessboard - Entry point
+* @a: char variable
+*
+*/
+
 void print_chessboard(char (*a)[8])
 {
-int i, x;
-for (i = 0; i <= 7; i++)
+	int i, j;
+
+	for (i = 0; i < 8; i++)
 	{
-	for (x = 0; x <= 7; x++)
-	_putchar(a[i][x]);
-_putchar('\n');
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
 	}
-}i
+}
