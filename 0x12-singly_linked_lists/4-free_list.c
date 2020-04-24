c3a17946da60bc7d4d5f6e4a6a4aa16a9ff68d7f
Saroco83 <estudialondres@gmail.com>
2020-04-24 09:46:11 +0000

    fix

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_list - entry point
+ * @head: list_t
+*/
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head;
+		free(head->str);
+		free(head);
+		head = tmp->next;
+	}
+	free(head);
+}
