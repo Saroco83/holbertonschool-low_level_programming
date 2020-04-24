b7a2ee99ca9c1cf2f2dc27c6aedb7e2a43807e9b
Saroco83 <estudialondres@gmail.com>
2020-04-24 07:14:59 +0000

    fix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,64 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-int funaux(int i);
-/**
- *funaux - coins counter.
- *@i: money.
- *Return: coin number.
- */
-int funaux(int i)
-{
-	int c, mc = 0;
-	int monedas[] = {25, 10, 5, 2, 1};
-	
-	for (c = 0; i != 0;)
-	{
-		fun:;
-		if (i - monedas[c] >= 0)
-		{
-			i = i - monedas[c];
-			mc++;
-		}
-		else
-		{
-			if (i > 0)
-			{
-				c++;
-				goto fun;
-			}
-		return (mc);
-		}
-	}
-	return (0);
-}
 
 /**
- *main -  prints the minimum number of coins to make change.
- *@argc: number of arguments passed.
- *@argv: pointer array which points to each argument passed.
- *Return: return 0 if all its good and 1 if not receive two arguments.
+ * main - Entry point
+ * @argc: int variable
+ * @argv: char* variable
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
-	int i;
+	int n, c = 0;
 
-	if (argc == 2)
-	{
-		i = atoi(argv[1]);
-		if (i < 0)
-		{
-			printf("0\n");
-		}
-		else
-		{
-			printf("%d\n", funaux(i));
-		}
-	}
-	else
+	if (argc < 2 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	else
+	{
+		n = atoi(argv[1]);
+		while (n > 0)
+		{
+			if (n >= 25)
+				n = n - 25;
+			else if (n >= 10)
+				n = n - 10;
+			else if (n >= 5)
+				n = n - 5;
+			else if (n >= 2)
+				n = n - 2;
+			else if (n >= 1)
+				n = n - 1;
+			c++;
+		}
+		printf("%d\n", c);
+	}
+
 	return (0);
 }
