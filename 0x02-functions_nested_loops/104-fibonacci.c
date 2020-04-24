d4b049b285140bb2b45e867c24336f6faa332d45
Saroco83 <estudialondres@gmail.com>
2020-04-24 03:46:47 +0000

    refreshed

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,26 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
- * main - function ppal
+ * main - Entry point
  *
- * Return: Always 0;
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
+	unsigned long x = 1, y = 2, z, vil, leo, ale, mil, dry, dar;
 	int i;
-	float e, f, g;
 
-	e = 1;
-	f = 2;
-
-	printf("%.0f, ", e);
-	printf("%.0f", f);
-	for (i = 0; i < 96; i++)
+	printf("1, 2, ");
+	for (i = 0; i < 90; i++)
 	{
-		g = e + f;
-		printf(", %.0f", g);
-		e = f;
-		f = g;
+		z = x + y;
+		printf("%lu, ", (unsigned long) z);
+		x = y;
+		y = z;
 	}
-	putchar('\n');
+	vil = x / 10000000000;
+	leo = x % 10000000000;
+	ale = y / 10000000000;
+	mil = y % 10000000000;
+	for (i = 90; i < 96; i++)
+	{
+		dry = vil + ale;
+		dar = leo + mil;
+		if (i == 91 || i == 92 || i == 94 || i == 95)
+		{
+			dar = dar - 10000000000;
+			dry = dry + 1;
+		}
+		if (i != 95)
+			printf("%lu%lu, ", (unsigned long) dry, (unsigned long) dar);
+		else
+			printf("%lu%lu\n", (unsigned long) dry, (unsigned long) dar);
+		vil = ale;
+		leo = mil;
+		ale = dry;
+		mil = dar;
+	}
+
 	return (0);
 }
