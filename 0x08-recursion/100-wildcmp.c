1dac426d749a2d4b8f4b04bf99fd150562988729
Saroco83 <estudialondres@gmail.com>
2020-04-24 07:06:35 +0000

    fix

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,31 +1,51 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
- * wildcmp - compares two strings
- * @s1: string
- * @s2: string.
+ * kill - Entry point
+ * @s1: char variable
+ * @s2: char variable
+ * @i: int variable
  *
- * Return: 1 on success.
- */
+ * Return: int
+*/
+
+int kill(char *s1, char *s2, int i)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s2 == '*')
+		return (kill(s1, s2 + 1, ++i));
+	else if (*s1 != *s2 && i > 0)
+	{
+		if (!*s1)
+			return (0);
+		return (kill(s1 + 1, s2, i));
+	}
+	else if (*s1 == *s2 && i == 0)
+		return (kill(s1 + 1, s2 + 1, 0));
+	else if (*s1 == *s2 && i > 0)
+	{
+		if (!kill(s1 + 1, s2 + 1, 0))
+			return (kill(s1 + 1, s2, i));
+		return (kill(s1 + 1, s2 + 1, i));
+	}
+	else if (*s1 != *s2 && i == 0)
+		return (0);
+	return (-1);
+}
+
+/**
+* wildcmp - Entry point
+* @s1: char variable
+* @s2: char variable
+*
+* Return: int
+*/
 
 int wildcmp(char *s1, char *s2)
 {
+	int i = 0;
 
-	if (s1 == s2 || (s1 == '\0' && s2 == '\0'))
-	{
-		return (1);
-	}
-
-	if (*s1 == *s2)
-	{
-		return (wildcmp(s1 + 1, s2 + 1));
-	}
-
-	if (*s2 == '*' && *s1 != '\0')
-	{
-		return (wildcmp(s1, s2 + 1));
-	}
-
-
-	return (0);
+	return (kill(s1, s2, i));
 }
