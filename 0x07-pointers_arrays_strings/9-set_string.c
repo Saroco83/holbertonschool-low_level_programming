bfbc550b32b809db6aa6975809b61722ade7ee0c
Saroco83 <estudialondres@gmail.com>
2020-04-24 06:26:23 +0000

    fix

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+* set_string - Entry point
+* @s: char variable
+* @to: char variable
+*
+* Return: dest
+*/
+
+void set_string(char **s, char *to)
+{
+	*(s) = to;
+}
