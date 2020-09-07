60ce495fc6c1d89f9e6152bde4ec94f7cb5ebffa
Saroco83 <estudialondres@gmail.com>
2020-09-07 05:06:53 +0000

    adding node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,14 +8,10 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
-	new = malloc()sizeof(dlistint_t);
-	if(new == NULL);
 
-	new->n = n;
-	new->prev = NULL;
-	new->next = *head;
-	if (new == NULL);
-		return(NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 	new->n = n;
 	new->prev = NULL;
 	new->next = *head;
