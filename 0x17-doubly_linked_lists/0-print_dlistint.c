8433b1e61741396a5ba07d9771cdae0057c6209e
Saroco83 <estudialondres@gmail.com>
2020-09-07 04:29:16 +0000

    adding node

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,14 +1,19 @@
 #include "lists.h"
-
+/**
+ * print_dlistint - check the code for Holberton School students.
+ * @h: argument input
+ * Return: nodes
+ */
 
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nodes = 0;
-	while(h)
+
+	while (h)
 	{
 		nodes = nodes + 1;
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-	return(nodes);
+	return (nodes);
 }
